ec730728ee450485e7831d8a661ddb7c2f03d597
Daniel Zwelling <dzwellingmusic@gmail.com>
2015-12-24 12:40:31 -0800

    Add email functionality

--- django_fun/django_fun/settings.py
+++ django_fun/django_fun/settings.py
@@ -27,6 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+EMAIL_HOST = 'smtp.yahoo.com'
+EMAIL_HOST_USER = 'littlez747@yahoo.com'
+EMAIL_HOST_PASSWORD = 'ThreeOver5@'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
 
 # Application definition
 
