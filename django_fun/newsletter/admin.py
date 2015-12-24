74c48714702e7449b5dc519706919475c7aae9ef
Daniel Zwelling <dzwellingmusic@gmail.com>
2015-12-23 21:09:54 -0800

    Add form to front end

--- django_fun/newsletter/admin.py
+++ django_fun/newsletter/admin.py
@@ -6,7 +6,7 @@ from .models import SignUp
 
 # Changes layout of admin page
 class SignUpAdmin(admin.ModelAdmin):
-    list_display = ["__str__", "timestamp", "updated"]
+    list_display = ["__str__", "full_name", "timestamp", "updated"]
     form = SignUpForm
     # class Meta:
     #     model = SignUp
--- django_fun/newsletter/views.py
+++ django_fun/newsletter/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
+from .forms import SignUpForm
 
 # Create your views here.
 def home(request):
     title = 'Welcome'
-    if request.user.is_authenticated():
-        title = 'My Title %s' % (request.user)
+    form = SignUpForm(request.POST or None)
     context = {
         'title': title,
-        'name': 'The Moose',
+        'form': form
     }
+
+    if form.is_valid():
+        instance = form.save(commit=False)
+        if not instance.full_name:
+            instance.full_name = "Mr. Krabs"
+        instance.save()
+        context = {
+            'title': 'Thank You'
+        }
     return render(request, "home.html", context)
